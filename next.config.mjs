9298cf6d823e420b390f8ab6ec3b7db904d75db1
ROKON💦💭 <130083848+rokonislam1520@users.noreply.github.com>
2026-04-22 10:45:40 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "valtio/vanilla": require.resolve("valtio/vanilla"),
+    };
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
